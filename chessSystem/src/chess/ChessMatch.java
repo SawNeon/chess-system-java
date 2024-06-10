3f8e7817b4c00b211d641fdd3441da036dd8a9fc
SawNeon <maheusbastost313@gmail.com>
2024-06-10 10:00:41 -0300

    Knight

--- chessSystem/src/chess/ChessMatch.java
+++ chessSystem/src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
+import chess.pieces.knight;
 
 public class ChessMatch {
 
@@ -196,6 +197,8 @@ public class ChessMatch {
 	
 	private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('b', 1, new knight(board, Color.WHITE));
+		placeNewPiece('g', 1, new knight(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
@@ -211,6 +214,8 @@ public class ChessMatch {
         
 
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('b', 8, new knight(board, Color.BLACK));
+		placeNewPiece('g', 8, new knight(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
